6ade8c3db59946a33b0527a1ce6ea07946b4cb62
Pavel Samarkin <samarkinpa@gmail.com>
2021-07-26 09:03:15 -0700

    Fix color output on Rapberry Pi
    Apparently, some terminals reset color on `%b` (end bold)

--- execution-time.plugin.zsh
+++ execution-time.plugin.zsh
@@ -28,17 +28,17 @@ format_time() {
   local sec=$((($1%60000)/1000))
   local msec=$(($1%1000))
   if [ "$1" -lt 10000 ]; then
-    echo "%F{green}%B$1%b ms.%f"
+    echo "%F{green}%B$1%b%f %F{green}ms.%f"
   elif [ "$1" -le 60000 ]; then
-    echo "%F{green}%B$sec%b s. %B$msec%b ms.%f"
+    echo "%F{green}%B$sec%b%f %F{green}s. %B$msec%b%f %F{green}ms.%f"
   elif [ "$1" -gt 60000 ] && [ "$1" -le 180000 ]; then
-    echo "%F{yellow}%B$min%b min. %B$sec%b s.%f"
+    echo "%F{yellow}%B$min%b%f %F{yellow}min. %B$sec%b%f %F{yellow}s.%f"
   else
     if [ "$hours" -gt 0 ]; then
       min=$(($min%60))
-      echo "%F{red}%B$hours%b h. %B$min%b min. %B$sec%b s.%f"
+      echo "%F{red}%B$hours%b%f %F{red}h. %B$min%b%f %F{red}min. %B$sec%b%f %F{red}s.%f"
     else
-      echo "%F{red}%B$min%b min. %B$sec%b s.%f"
+      echo "%F{red}%B$min%b%f %F{red}min. %B$sec%b%f %F{red}s.%f"
     fi
   fi
 }
