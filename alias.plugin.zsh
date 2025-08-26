0229888bb6e351cc3bc90094301abd1b52cc7070
Pavel Samarkin <samarkinpa@gmail.com>
2025-08-25 22:48:31 -0700

    Make `ls` alias universal

--- alias.plugin.zsh
+++ alias.plugin.zsh
@@ -1,4 +1,11 @@
-alias ls='ls -F --color=auto'
+if which eza > /dev/null 2>&1; then
+  alias ls='eza -F'
+elif ls --version 2>&1 | grep -q 'GNU\|BusyBox'; then
+  alias ls='ls -F --color=auto'
+else
+  alias ls='ls -FG'
+fi
+
 alias grep='grep --color=auto'
 alias cd..='cd ..'
 alias git-graph='git log --graph --oneline --all'
