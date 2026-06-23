d909d1eb974c637dd8d0aedb59613a8f5c272f45
Pavel Samarkin <samarkinpa@gmail.com>
2026-06-23 00:35:59 -0700

    Add 0-padding to current hour in prompt

--- prompt-line.plugin.zsh
+++ prompt-line.plugin.zsh
@@ -1,4 +1,4 @@
 # Set prompt to current date and time, colored full path in the scp-compatible format, and last exitcode on the new line
 
-PS1="(%D %*) %F{green}%n%f@%F{${ZSH_PROMPT_COLOR:-yellow}}%m%f:%F{magenta}%0~%f
+PS1="(%D{%y-%m-%d %H:%M:%S}) %F{green}%n%f@%F{${ZSH_PROMPT_COLOR:-yellow}}%m%f:%F{magenta}%0~%f
 %(?.%F{green}√.%F{red}<%?>)%f %(!.#.$) "
