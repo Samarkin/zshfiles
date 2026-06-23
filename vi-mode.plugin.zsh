c3537b883ddc56e43ea8fd94226c04760cf881d1
Pavel Samarkin <samarkinpa@gmail.com>
2026-06-22 23:05:10 -0700

    Add VI cursors for Linux terminal

--- vi-mode.plugin.zsh
+++ vi-mode.plugin.zsh
@@ -2,11 +2,22 @@
 
 KEYTIMEOUT=5 # Remove delays when switching modes
 
+# Linux console uses private cursor codes (ESC[?Nc); GUI terminals use DECSCUSR (ESC[N q)
+if [[ $TERM == linux ]]; then
+  _vi_mode_block='\e[?6c' # Full block for NORMAL mode
+  _vi_mode_under='\e[?2c' # Underscore for INSERT mode
+  _vi_mode_reset='\e[?0c' # Default cursor before executing any command
+else
+  _vi_mode_block='\e[2 q' # Steady block for NORMAL mode
+  _vi_mode_under='\e[4 q' # Steady underscore for INSERT mode
+  _vi_mode_reset='\e[0 q' # Default cursor before executing any command
+fi
+
 function zle-keymap-select zle-line-init {
   if [ $KEYMAP = vicmd ]; then
-    echo -ne '\e[2 q' # Block for NORMAL mode
+    echo -ne $_vi_mode_block
   else
-    echo -ne '\e[4 q' # Underscore for INSERT mode
+    echo -ne $_vi_mode_under
   fi
 }
 zle -N zle-keymap-select # Execute on each keymap change
@@ -17,6 +28,6 @@ bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
 
 _vi_mode_preexec() {
-    echo -ne '\e[0 q' # Reset to default before executing any command
+    echo -ne $_vi_mode_reset # Reset to default before executing any command
 }
 preexec_functions+=(_vi_mode_preexec)
